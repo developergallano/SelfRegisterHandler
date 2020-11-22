198df56dcf639b618650de1315bde4e650646edb
allan <allan@localhost.localdomain>
2020-11-22 13:38:42 -0700

    install extensions

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -8,6 +8,30 @@ fi
 readonly dockername="cpp-builder-inst"
 readonly dockerimage="cpp-builder"
 
+function install_code_extensions()
+{
+    code --install-extension ms-vscode-remote.remote-containers
+    code --install-extension ms-vscode-remote.remote-ssh
+    code --install-extension ms-vscode-remote.remote-ssh-edit
+    code --install-extension ms-vscode-remote.remote-wsl
+    code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+    code --install-extension ms-vscode.cmake-tools
+    code --install-extension ms-vscode.cpptools
+    code --install-extension ryu1kn.partial-diff
+    code --install-extension tht13.html-preview-vscode
+    code --install-extension twxs.cmake
+    code --install-extension vikas.code-navigation
+}
+
+function docker_build()
+{
+    image_exist=$( docker images | grep cpp-builder)
+    if [ -z "$image_exist" ]; then
+        echo "building  docker image $dockerimage"
+        docker build -t $dockerimage .    
+    fi
+}
+
 function start_docker()
 {
     docker volume create userhome
@@ -33,9 +57,15 @@ function exe_args()
 
 function main_func()
 {
+    # code extension first
+    install_code_extensions
+
     running=$(docker ps -s -f name=$dockername -q)
     if [ -z "$running" ]; then
+        
         echo "Docker $dockername is NOT running"
+        docker_build
+
         start_docker
     fi
 
---
+++ installExtension.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ryu1kn.partial-diff
+code --install-extension tht13.html-preview-vscode
+code --install-extension twxs.cmake
+code --install-extension vikas-code-navigation
+
